390fef5d5595b9a4123426454d0a8363f58556dc
Enchan <enchant0130@gmail.com>
2020-12-24 22:45:29 +0900

    [Add] Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DBAccessLib
+
+## Overview
+
+DB Access Library for Python.
+
+## Licence
+
+All files in this repository is published under the MIT Licence.  
+In details, see `LICENCE`.
---
+++ setup.py
@@ -0,0 +1,20 @@
+#
+# pipが読んでライブラリの諸々を設定するためのファイル
+#
+import glob
+import setuptools, os
+
+setuptools.setup(
+    name="DBAccessLib",
+    version="0.1.0",
+    license="MIT Licence",
+    description="Database access interface library",
+    author="Enchan1207",
+    url="https://github.com/Enchan1207/DBAccessLib",
+    packages=setuptools.find_packages("src"),
+    install_requires=["mysql-connector-python"],
+    package_dir={"": "src"},
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False
+)
